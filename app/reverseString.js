9252f2846bf01288f4ab7ed21b93759f7dc02d86
Michael Eboagu <Michael Eboagu>
2017-05-11 12:23:14 +0100

    all tests passed

--- app/reverseString.js
+++ app/reverseString.js
@@ -2,12 +2,14 @@ module.exports = {
 
 	reverseString: (word) =>{
 
-		//if(string == ''){
-			//return null;
-		//}
+		if(word === ''){
+			return null;
+		}
+		
 		let reversedWord = "";
+		
 		for(let counter = (word.length -1); counter >= 0 ; counter--){
-		   //console.log(string[i]);
+		  
 		   reversedWord += word[counter];
 		}
 		if(reversedWord === word){
