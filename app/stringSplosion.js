3af9c9092148cfcce23b19f41c4ea5b2ea8d4a47
Michael Eboagu <Michael Eboagu>
2017-05-11 12:08:32 +0100

    add stringSplosion file

--- app/stringSplosion.js
+++ app/stringSplosion.js
@@ -1,5 +1,31 @@
 class StringSplosion{
 	constructor(word){
 		this.word = word;
+		this.letters = this.word.split('');
+		console.log(this.letters);
 	}
+
+	manipulate(){
+		let stop = 0;
+		let start = 0;
+	    this.sploded = [];
+		while(stop <= this.letters.length){
+			let val = this.letters.slice(start,stop);
+			console.log(val);
+			this.sploded.push(val);
+			console.log(this.sploded);
+			stop++;
+		}
+		//this.sploded.split(',');
+		let sploded = this.sploded.toString();
+		sploded = sploded.replace(/,/g,'');
+		
+		return sploded;
+	}
+
 }
+
+module.exports = StringSplosion;
+
+let me = new StringSplosion("like");
+me.turnToString(me.manipulate());
